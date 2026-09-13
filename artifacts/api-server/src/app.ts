6f0b317e79d0e0a7f9884d76092b5fef2f7ead3a
cvavveti <cvavveti@gmail.com>
2026-09-13 12:11:30 +0530

    Refactor pino-http import and serializers

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,23 +1,26 @@
 import express, { type Express } from "express";
 import cors from "cors";
-import { pinoHttp } from "pino-http";
+import * as pinoHttpModule from "pino-http";
 import router from "./routes";
 import { logger } from "./lib/logger";
 
 const app: Express = express();
 
+const pinoHttp: any =
+  (pinoHttpModule as any).default ?? pinoHttpModule;
+
 app.use(
   pinoHttp({
     logger,
     serializers: {
-      req(req) {
+      req(req: any) {
         return {
           id: req.id,
           method: req.method,
           url: req.url?.split("?")[0],
         };
       },
-      res(res) {
+      res(res: any) {
         return {
           statusCode: res.statusCode,
         };
